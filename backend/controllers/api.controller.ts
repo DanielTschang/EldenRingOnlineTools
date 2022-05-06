a0d0c1ef0c460398fe77699f4ba2392c354f70af
danchang11 <cssource@livemail.tw>
2022-05-06 16:18:14 +0800

    add some doc for api controller

--- backend/controllers/api.controller.ts
+++ backend/controllers/api.controller.ts
@@ -5,24 +5,36 @@ import * as MarkerAPIService from "./utils/api.queries";
 
 class ApiController {
 
-
+  /**
+   * Get marker data by marker id
+   * 
+   * @param req 
+   * @param res 
+   */
   public async getMarkerById(req: IGetMarkerReq, res: Response):Promise<void> {
     try{
-      let markerType:number = req.params.id
-      const markers = await MarkerAPIService.getMarkerById(markerType);
+      let markerId:number = req.params.id
+      const markers = await MarkerAPIService.getMarkerById(markerId);
       res.status(200).json({
-        "type": markerType,
+        "type": "id",
         "data": markers,
       })
     }
     catch(error){
-      console.error('[api.controller][getAlldata][Error] ', typeof error === 'object' ? JSON.stringify(error) : error);
+      console.error('[api.controller][getMarkerById][Error] ', typeof error === 'object' ? JSON.stringify(error) : error);
       res.status(500).json({
-        message: 'There was an error when fetching markerdata'
+        message: 'There was an error when fetching markerdata by id'
       });
     }
   }
 
+  /**
+   * Get marker data by type
+   * e.g : siteofgrace, deathroot...
+   * 
+   * @param req Express Resquest
+   * @param res Express Response
+   */
   public async getMarkerByType(req: IGetMarkerReq, res: Response):Promise<void> {
     try{
       let markerType:string = req.params.type
@@ -33,13 +45,19 @@ class ApiController {
       })
     }
     catch(error){
-      console.error('[api.controller][getAlldata][Error] ', typeof error === 'object' ? JSON.stringify(error) : error);
+      console.error('[api.controller][getMarkerByType][Error] ', typeof error === 'object' ? JSON.stringify(error) : error);
       res.status(500).json({
-        message: 'There was an error when fetching markerdata'
+        message: 'There was an error when fetching markerdata by type'
       });
     }
   }
 
+  /**
+   * Get all marker data
+   * 
+   * @param req Express Resquest
+   * @param res Express Response
+   */
   public async all(req: IGetMarkerReq, res: Response) {
     try{
       const markers = await MarkerAPIService.getAllData();
@@ -78,8 +96,8 @@ class ApiController {
 
   /**
    * update Marker data
-   * @param req 
-   * @param res 
+   * @param req Express Resquest
+   * @param res Express Response
    */
   public async updateMarkerById(req: IUpdateMarkerReq, res: Response){
     try{
