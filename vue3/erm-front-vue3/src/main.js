d7547e388909e72ca8266d486c3124d6ced41dd2
danchang11 <cssource@livemail.tw>
2022-05-17 21:26:35 +0800

    add leaflet sidebar

--- vue3/erm-front-vue3/src/main.js
+++ vue3/erm-front-vue3/src/main.js
@@ -3,4 +3,25 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from "@fortawesome/free-solid-svg-icons"
+
+
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+
+
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas,fab)
+/* add icons to the library */
+
+/* add font awesome icon component */
+
+createApp(App)
+    .component("fa",FontAwesomeIcon)
+    .use(router)
+    .mount('#app')
--- vue3/erm-front-vue3/src/views/MapView.vue
+++ vue3/erm-front-vue3/src/views/MapView.vue
@@ -1,40 +1,110 @@
+
+
 <template>
-  <div id="mymap" class="h-full z-10">
-  </div>
+    <div id="map-container" >
+    <div id="sidebar" class="leaflet-sidebar collapsed">
+
+        <!-- nav tabs -->
+        <div class="leaflet-sidebar-tabs">
+            <!-- top aligned tabs -->
+            <ul role="tablist">
+                <li><a href="#home" role="tab"><i class="fa fa-bars active"></i></a></li>
+                <li><a href="#autopan" role="tab"><i class="fa fa-arrows"></i></a></li>
+            </ul>
+
+            <!-- bottom aligned tabs -->
+            <ul role="tablist">
+                <li role="tab"><a href="https://github.com/nickpeihl/leaflet-sidebar-v2"><i class="fa fa-github"></i></a></li>
+            </ul>
+        </div>
+
+        <!-- panel content -->
+        <div class="leaflet-sidebar-content">
+            <div class="leaflet-sidebar-pane" id="home">
+                <h1 class="leaflet-sidebar-header">
+                    zh-TW Elden Ring Online Map
+                    <span class="leaflet-sidebar-close"><i class="fa fa-caret-left"></i></span>
+                </h1>
+
+                <p>A responsive sidebar for the mapping library <a href="https://leafletjs.com/">Leaflet</a>.</p>
+                <p>Compatible with version 0.7 and 1.x (tested up to 1.6.0)</p>
+                <p><b>Select the other panes for a showcase of each feature.</b></p>
+
+                <h2>More examples</h2>
+                <ul>
+                    <li><a href="./position-right.html">Right aligned</a></li>
+                    <li><a href="./halfheight.html">The sidebar adapts to map container size</a></li>
+                    <li><a href="./leaflet-latest.html">Proof that it works with the latest leaflet version (if not, please report it!)</a></li>
+                    <li><a href="./leaflet-0.7.html">Proof that it works with leaflet 0.7</a></li>
+                </ul>
+
+                <p class="lorem">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                <p class="lorem">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                <p class="lorem">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                <p class="lorem">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+            </div>
+
+            <div class="leaflet-sidebar-pane" id="autopan">
+                <h1 class="leaflet-sidebar-header">
+                    autopan
+                    <span class="leaflet-sidebar-close"><i class="fa fa-caret-left"></i></span>
+                </h1>
+                <p>
+                    <code>Leaflet.control.sidebar({ autopan: true })</code>
+                    makes shure that the map center always stays visible.
+                </p>
+                <p>
+                    The autopan behviour is responsive as well.
+                    Try opening and closing the sidebar from this pane!
+                </p>
+            </div>
+
+            <div class="leaflet-sidebar-pane" id="messages">
+                <h1 class="leaflet-sidebar-header">Messages<span class="leaflet-sidebar-close"><i class="fa fa-caret-left"></i></span></h1>
+            </div>
+        </div>
+    </div>
+
+        <div id="mymap" class="h-full z-10">
+        </div>
+    </div>
 </template>
+
+
 
 <script>
 // @ is an alias to /src
-import L from "leaflet";
+import * as L from "leaflet";
+import "leaflet-sidebar-v2";
+import "leaflet-sidebar-v2/css/leaflet-sidebar.css";
+
+
 export default {
     name: 'HomeView',
 
     data(){
         return {
+            zoom:3,
+            markers:[],
             maxZoom:7,
             minZoom:2,
             tileSize:200,
             zoomOffset:0,
-            maxBounds: [
-                [-200, 200],
-                [200, -200]
-            ],
-            maxBoundsBywin:L.latLngBounds(L.latLng(-100, -200), L.latLng(100, 100)),
+            initCenterLat:40,
+            initCenterLng:-40,
+            maxBounds:L.latLngBounds(L.latLng(-200, 200), L.latLng(200, -200)),
         }
     },
     mounted(){
-        var littleton = L.marker([0, 0]).bindPopup('This is Littleton, CO.'),
-            denver    = L.marker([39.74, 0]).bindPopup('This is Denver, CO.'),
-            aurora    = L.marker([39.73, 4.8]).bindPopup('This is Aurora, CO.'),
-            golden    = L.marker([39.77, 5.23]).bindPopup('This is Golden, CO.');
-        var cities = L.layerGroup([littleton, denver, aurora, golden]);
-        var none = L.layerGroup([]);
+        
+        let underground = L.tileLayer('https://imgs.ali213.net/picfile/eldenring_dx/{z}/{x}/{y}.png', {
+            attribution: '',
+            maxZoom: this.maxZoom,
+            id: 'underground',
+            tileSize: this.tileSize,
+            zoomOffset:this.zoomOffset
+        })
 
-
-        var overlayMaps = {
-            "Cities": cities,
-            "None": none
-        };
         let ground = L.tileLayer('https://imgs.ali213.net/picfile/eldenring/{z}/{x}/{y}.jpg', {
             attribution: 'Elden Ring Map',
             maxZoom: this.maxZoom,
@@ -43,28 +113,30 @@ export default {
             zoomOffset:this.zoomOffset,
             
         })
-        let underground = L.tileLayer('https://imgs.ali213.net/picfile/eldenring_dx/{z}/{x}/{y}.png', {
-            attribution: '',
-            maxZoom: this.maxZoom,
-            id: 'underground',
-            tileSize: this.tileSize,
-            zoomOffset:this.zoomOffset
-        })
-        // var baseMaps = {
-        //     "UnderGround": underground,
-        //     "Ground": ground
-        // };
+
+
         let mymap = L.map("mymap", {
             maxZoom:this.maxZoom,
             minZoom:this.minZoom,
             attributionControl: false, 
             zoomControl: false, 
             maxBounds: this.maxBounds,
-            layers : [underground,ground]
-        }).setView([0,0],4);
+            renderer: L.canvas(),
+            preferCanvas: true,
+            layers: [ground,underground]
+        }).setView([this.initCenterLat,this.initCenterLng],this.zoom);
 
+        var littleton = L.marker([39.61, -105.02]).bindPopup('This is Littleton, CO.'),
+            denver    = L.marker([39.74, -104.99]).bindPopup('This is Denver, CO.'),
+            aurora    = L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.'),
+            golden    = L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.');
 
-        L.control.layers(overlayMaps).addTo(mymap);
+        var cities = L.layerGroup([littleton, denver, aurora, golden]);
+
+        var overlayMaps = {
+            "Cities": cities
+        };
+        
         L.control.zoom({ position: 'topright' }).addTo(mymap);
         L.control.attribution({
             position: 'bottomright',
@@ -78,14 +150,55 @@ export default {
                 </a>
                 `,
             })
-      .addTo(mymap);
+        .addTo(mymap);
+
+        var baseMaps = {
+            "UnderGround": underground,
+            "Ground": ground
+        };
+
+        var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(mymap);
+
+        var crownHill = L.marker([39.75, -105.09]).bindPopup('This is Crown Hill Park.'),
+            rubyHill = L.marker([39.68, -105.00]).bindPopup('This is Ruby Hill Park.');
+
+        var parks = L.layerGroup([crownHill, rubyHill]);
+        
+        layerControl.addOverlay(parks, "Parks");
+        L.control.sidebar({
+            autopan: false, // whether to maintain the centered map point when opening the sidebar
+            closeButton: true, // whether t add a close button to the panes
+            container: "sidebar", // the DOM container or #ID of a predefined sidebar container that should be used
+            position: "left" // left or right
+            })
+        .addTo(mymap);
+
+
     }
 }
 </script>
 
 <style scoped>
-    #mymap{
-        height: 80vh;
-        width: 80vw
+    .leaflet-container{
+        background-color: #222222;
+    }
+    #map-container{
+        height: 100vh;
+        width: 100vw;
+        background:#222222;
+    }
+    #map {
+        height: 100%;
+        font: 10pt "Helvetica Neue", Arial, Helvetica, sans-serif;
+    }
+    
+
+    .icon-container{
+        margin:0px auto;
+    }
+    .lorem {
+        font-style: italic;
+        text-align: justify;
+        color: #AAA;
     }
 </style>
