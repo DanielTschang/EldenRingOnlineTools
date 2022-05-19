820847643be66f4d95bb17fd9bde57b09d8d9f34
danchang11 <cssource@livemail.tw>
2022-05-20 04:43:28 +0800

    change icon size (smaller better)

--- vue3/erm-front-vue3/src/utils/markerIcon.js
+++ vue3/erm-front-vue3/src/utils/markerIcon.js
@@ -4,30 +4,30 @@ import iconurl from './IconUrl.json'
 export default {
     "SiteOfGrace":L.icon({
         iconUrl: iconurl.SiteOfGrace,
-        iconSize:     [35, 35], // size of the icon
+        iconSize:     [25, 25], // size of the icon
     }),
     "shortPath":L.icon({
         iconUrl: iconurl.shortPath,
-        iconSize:     [35, 35],
+        iconSize:     [25, 25],
     }),
     "Waygates":L.icon({
         iconUrl: iconurl.Waygates,
-        iconSize:     [35, 35],
+        iconSize:     [25, 25],
     }),
     "RuneFarm":L.icon({
         iconUrl: iconurl.RuneFarm,
-        iconSize:     [35, 35],
+        iconSize:     [25, 25],
     }),
     "Shop":L.icon({
         iconUrl: iconurl.Shop,
-        iconSize:     [35, 35],
+        iconSize:     [25, 25],
     }),
     "NPC":L.icon({
         iconUrl: iconurl.NPCIcon,
-        iconSize:     [35, 35],
+        iconSize:     [25, 25],
     }),
     "Location":L.icon({
         iconUrl: iconurl.Location,
-        iconSize:     [35, 35],
+        iconSize:     [25, 25],
     }),
 }
