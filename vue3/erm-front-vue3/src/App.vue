cae37befda6f3be22878f9f16b54e01dea17aed7
danchang11 <cssource@livemail.tw>
2022-05-20 04:44:21 +0800

    add router

--- vue3/erm-front-vue3/src/App.vue
+++ vue3/erm-front-vue3/src/App.vue
@@ -1,7 +1,26 @@
+
 <template>
-  <router-view/>
+  <div class="MainContainer">
+    <div class="routerview" style="margin-bottom: 7vh">
+      <router-view/>
+    </div>
+    <SideBar/>
+  </div>
 </template>
 
+
+
+<script>
+import SideBar from "@/components/sidebar/SideBar.vue"
+export default {
+  components:{
+    SideBar
+  },
+  setup() {
+    
+  },
+}
+</script>
 
 
 <style lang="scss">
@@ -12,6 +31,16 @@
     text-align: center;
     color: #2c3e50;
   }
+  :root{
+    --sidebar-bg-color:green;
+    --sidebar-bg-item-hover:white;
+    --sidebar-bg-item-active:black;
+  }
+  .MainContainer{
+    display: flex;
+    flex-direction: column;
+  }
+
 
   nav {
     padding: 30px;
@@ -26,3 +55,4 @@
     }
   }
 </style>
+
