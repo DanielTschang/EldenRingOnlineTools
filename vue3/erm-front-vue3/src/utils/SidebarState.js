c789513e4a5f22b541bd6d1906517c0a628098f6
danchang11 <cssource@livemail.tw>
2022-05-20 04:43:43 +0800

    Store Side bar State

---
+++ vue3/erm-front-vue3/src/utils/SidebarState.js
@@ -0,0 +1,9 @@
+import {ref, computed} from 'vue';
+
+export const collapsed = ref(false);
+export const toggleSidebar = () => (collapsed.value = !collapsed.value);
+export const SIDEBAR_WIDTH = 180;
+export const SIDEBAR_WIDTH_COLLAPSED = 30 ;
+export const sidebarWidth = computed(
+    ()=> `${collapsed.value ? SIDEBAR_WIDTH_COLLAPSED : SIDEBAR_WIDTH}px`
+)
