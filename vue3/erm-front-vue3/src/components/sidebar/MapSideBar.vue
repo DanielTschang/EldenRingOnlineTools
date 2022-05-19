5f223b9f52a0390a74f451855e2c53a41073caa2
danchang11 <cssource@livemail.tw>
2022-05-20 04:44:11 +0800

    Map SideBar, plan to be a marker checkbox

---
+++ vue3/erm-front-vue3/src/components/sidebar/MapSideBar.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="sidebar" :style="{width:sidebarWidth}">
+        <span
+            class="collapse-icon"
+            @click="toggleSidebar"
+        >
+            <i class="fa fa-angle-double-left"></i>
+        </span>
+  </div>
+</template>
+
+<script>
+import { collapsed, toggleSidebar, sidebarWidth } from "@/utils/SidebarState";
+
+export default {
+    name:"MapSideBar",
+    props:{},
+    setup(){
+        return { collapsed, toggleSidebar, sidebarWidth }
+    }
+}
+  
+</script>
+
+<style >
+  :root{
+    --sidebar-bg-color:black;
+    --sidebar-item-hover:white;
+    --sidebar-item-active:green;
+  }
+
+</style>
+
+<style scoped>
+  .sidebar {
+    color:white;
+    background-color: black;
+
+    float:right;
+    position:fixed;
+    z-index:10000;
+    left:0;
+    bottom:0;
+    top:5vh;
+    width: 7vw;
+    height: 80vh;
+    border-end-end-radius: 20px;
+    
+    transition: 0.3s ease;
+
+    display: flex;
+    flex-direction: column
+ }
+</style>
