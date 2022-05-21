73bbb19bb2d68d4690b83a05c21919d2b66ee21c
danchang11 <cssource@livemail.tw>
2022-05-22 05:09:26 +0800

    fix typo & change icon size

--- vue3/erm-front-vue3/src/utils/markerIcon.js
+++ vue3/erm-front-vue3/src/utils/markerIcon.js
@@ -1,33 +1,219 @@
 
 import * as L from "leaflet";
 import iconurl from './IconUrl.json'
+const IconsSize = [35,35]
 export default {
     "SiteOfGrace":L.icon({
         iconUrl: iconurl.SiteOfGrace,
-        iconSize:     [25, 25], // size of the icon
+        iconSize:     IconsSize, // size of the icon
     }),
     "shortPath":L.icon({
         iconUrl: iconurl.shortPath,
-        iconSize:     [25, 25],
+        iconSize:     IconsSize,
     }),
     "Waygates":L.icon({
         iconUrl: iconurl.Waygates,
-        iconSize:     [25, 25],
+        iconSize:     IconsSize,
     }),
     "RuneFarm":L.icon({
         iconUrl: iconurl.RuneFarm,
-        iconSize:     [25, 25],
+        iconSize:     IconsSize,
     }),
     "Shop":L.icon({
         iconUrl: iconurl.Shop,
-        iconSize:     [25, 25],
+        iconSize:     IconsSize,
     }),
     "NPC":L.icon({
-        iconUrl: iconurl.NPCIcon,
-        iconSize:     [25, 25],
+        iconUrl: iconurl.NPC,
+        iconSize:     IconsSize,
     }),
     "Location":L.icon({
         iconUrl: iconurl.Location,
-        iconSize:     [25, 25],
+        iconSize:     IconsSize,
     }),
+    "SummoningPool":L.icon({
+        iconUrl: iconurl.SummoningPool,
+        iconSize:     IconsSize,
+    }),
+    "Cave":L.icon({
+        iconUrl: iconurl.Cave,
+        iconSize:     IconsSize,
+    }),
+    "HorseTorrent":L.icon({
+        iconUrl: iconurl.HorseTorrent,
+        iconSize:     IconsSize,
+    }),
+    "WalkingMausoleum":L.icon({
+        iconUrl: iconurl.WalkingMausoleum,
+        iconSize:     IconsSize,
+    }),
+    "BigBoss":L.icon({
+        iconUrl: iconurl.BigBoss,
+        iconSize:     IconsSize,
+    }),
+    "Boss":L.icon({
+        iconUrl: iconurl.Boss,
+        iconSize:     IconsSize,
+    }),
+    "LittleBoss":L.icon({
+        iconUrl: iconurl.LittleBoss,
+        iconSize:     IconsSize,
+    }),
+    "NPCInvaders":L.icon({
+        iconUrl: iconurl.NPCInvaders,
+        iconSize:     IconsSize,
+    }),
+    "GreatEnemy":L.icon({
+        iconUrl: iconurl.GreatEnemy,
+        iconSize:     IconsSize,
+    }),
+    "Item":L.icon({
+        iconUrl: iconurl.Item,
+        iconSize:     IconsSize,
+    }),
+    "Cartacombs":L.icon({
+        iconUrl: iconurl.Cartacombs,
+        iconSize:     IconsSize,
+    }),
+    "Evergaol":L.icon({
+        iconUrl: iconurl.Evergaol,
+        iconSize:     IconsSize,
+    }),
+    "GoldenSeed":L.icon({
+        iconUrl: iconurl.GoldenSeed,
+        iconSize:     IconsSize,
+    }),
+    "CrystalTears":L.icon({
+        iconUrl: iconurl.CrystalTears,
+        iconSize:     IconsSize,
+    }),
+    "KeyItems":L.icon({
+        iconUrl: iconurl.KeyItems,
+        iconSize:     IconsSize,
+    }),
+    "StoneSwordKey":L.icon({
+        iconUrl: iconurl.StoneSwordKey,
+        iconSize:     IconsSize,
+    }),
+    "DeathRoot":L.icon({
+        iconUrl: iconurl.DeathRoot,
+        iconSize:     IconsSize,
+    }),
+    "DragonHeart":L.icon({
+        iconUrl: iconurl.DragonHeart,
+        iconSize:     IconsSize,
+    }),
+    "LarvalTear":L.icon({
+        iconUrl: iconurl.LarvalTear,
+        iconSize:     IconsSize,
+    }),
+    "PickersBellBearing":L.icon({
+        iconUrl: iconurl.PickersBellBearing,
+        iconSize:     IconsSize,
+    }),
+    "SacredTear":L.icon({
+        iconUrl: iconurl.SacredTear,
+        iconSize:     IconsSize,
+    }),
+    "MinersBellBearing":L.icon({
+        iconUrl: iconurl.MinersBellBearing,
+        iconSize:     IconsSize,
+    }),
+    "Painting":L.icon({
+        iconUrl: iconurl.Painting,
+        iconSize:     IconsSize,
+    }),
+    "Gesture":L.icon({
+        iconUrl: iconurl.Gesture,
+        iconSize:     IconsSize,
+    }),
+    "Material":L.icon({
+        iconUrl: iconurl.Material,
+        iconSize:     IconsSize,
+    }),
+    "Sorceries":L.icon({
+        iconUrl: iconurl.Sorceries,
+        iconSize:     IconsSize,
+    }),
+    "Weapon":L.icon({
+        iconUrl: iconurl.Weapon,
+        iconSize:     IconsSize,
+    }),
+    "Cookbook":L.icon({
+        iconUrl: iconurl.Cookbook,
+        iconSize:     IconsSize,
+    }),
+    "Whetblade":L.icon({
+        iconUrl: iconurl.Whetblade,
+        iconSize:     IconsSize,
+    }),
+    "Incantation":L.icon({
+        iconUrl: iconurl.Incantation,
+        iconSize:     IconsSize,
+    }),
+    "Tailsman":L.icon({
+        iconUrl: iconurl.Tailsman,
+        iconSize:     IconsSize,
+    }),
+    "AshOfWar":L.icon({
+        iconUrl: iconurl.AshOfWar,
+        iconSize:     IconsSize,
+    }),
+    "SpiritAsh":L.icon({
+        iconUrl: iconurl.SpiritAsh,
+        iconSize:     IconsSize,
+    }),
+    "Armor":L.icon({
+        iconUrl: iconurl.Armor,
+        iconSize:     IconsSize,
+    }),
+    "Warn":L.icon({
+        iconUrl: iconurl.Warn,
+        iconSize:     IconsSize,
+    }),
+    "Taoke":L.icon({
+        iconUrl: iconurl.Taoke,
+        iconSize:     IconsSize,
+    }),
+    "SmithingStone":L.icon({
+        iconUrl: iconurl.SmithingStone,
+        iconSize:     IconsSize,
+    }),
+    "SomberSmithingStone":L.icon({
+        iconUrl: iconurl.SomberSmithingStone,
+        iconSize:     IconsSize,
+    }),
+    "Question":L.icon({
+        iconUrl: iconurl.Question,
+        iconSize:     IconsSize,
+    }),
+    "MAP":L.icon({
+        iconUrl: iconurl.MAP,
+        iconSize:     IconsSize,
+    }),
+    "Text":L.icon({
+        iconUrl: iconurl.Text,
+        iconSize:     IconsSize,
+    }),
+    "GravePickerBellBearing":L.icon({
+        iconUrl: iconurl.GravePickerBellBearing,
+        iconSize:     IconsSize,
+    }),
+    "GhostPickerBellBearing":L.icon({
+        iconUrl: iconurl.GhostPickerBellBearing,
+        iconSize:     IconsSize,
+    }),
+    "BellBearing":L.icon({
+        iconUrl: iconurl.BellBearing,
+        iconSize:     IconsSize,
+    }),
+    "SmithingBellBearing":L.icon({
+        iconUrl: iconurl.SmithingBellBearing,
+        iconSize:     IconsSize,
+    }),
+    "SomberBellBearing":L.icon({
+        iconUrl: iconurl.SomberBellBearing,
+        iconSize:     IconsSize,
+    }),
+
 }
