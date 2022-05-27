63988892aa80d9abd20897353ae23bc31bf0cabd
danchang11 <cssource@livemail.tw>
2022-05-28 00:53:19 +0800

    InfoView init

---
+++ vue3/erm-front-vue3/src/views/InfoView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <h1>yo</h1>
+      <button @click="LogIn">yoyo</button>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue"
+
+
+// eslint-disable-next-line
+const isOpened = ref(false)
+export default {
+    name:"InfoView",
+    methods:{
+        LogIn(){
+            this.$emit('LogInTrigger')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
