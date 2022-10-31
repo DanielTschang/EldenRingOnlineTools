a5c90609daf7c926c05dd97f082bba6be43106be
danchang11 <cssource@livemail.tw>
2022-10-31 23:58:51 +0800

    define the interface of a weapon

--- backend/temp.ts
+++ backend/temp.ts
@@ -65,8 +65,68 @@ export const allWeaponTypes = [
   "Whip",
 ] as const;
 
+/*
+Define the min & max level of regular/special weapons
+*/
 export const minWeaponUpgradedLevel = 0 as const;
 export const maxRegularWeaponUpgradedLevel = 25 as const;
 export const maxSpecialWeaponUpgradedLevel = 10 as const;
 
+/*
+Define the min & max level of a each status
+*/
+export const minStatusLevel = 1 as const;
+export const maxStatusLevel = 99 as const;
 
+/*
+Define the min & max level of a character
+*/
+export const minCharacterLevel = 1 as const;
+export const maxCharacterLevel = 713 as const;
+
+
+export type Attribute = typeof allAttributes[number];
+export type Affinitie = typeof allAffinities[number];
+export type AttackType = typeof allAttackTypes[number];
+export type PassiveEffect = typeof allPassiveEffects[number];
+
+export interface weaponAttackResult {
+    attackRating : number;
+}
+
+export interface Weapon{
+    /*
+    Full name of the weapon
+    */
+    weaponName : string;
+
+    /*
+    Regular/Special weapon? (+10 or +25)
+    */
+    isSpecial : boolean;
+
+    /*
+    Character attribute requirements to use the weapon without penalty
+    */
+    attributeRequirement : Record<Attribute, number>;
+
+    /*
+    Scaling amount for attribute
+    */
+    attributeScaling : Record<Attribute, number>;
+
+    /*
+    indicating the base attack rating for each attack type
+    */
+    baseAttack : Record<AttackType, number>;
+
+    /*
+        indicating the base passive effect of the weapon, ­t­±ª¬ºA 
+    */
+    passiveEffect : Record<PassiveEffect, number>;
+}
+
+export interface weaponInfo {
+    weaponWeight : number;
+    weaponLevel : number;
+}
---
+++ backend/weapon.ts
@@ -0,0 +1,38 @@
+import { AttackType, Attribute, PassiveEffect } from "./temp";
+
+export interface Weapon{
+    /*
+    Full name of the weapon
+    */
+    weaponName : string;
+
+    /*
+    Regular/Special weapon? (+10 or +25)
+    */
+    isSpecial : boolean;
+
+    /*
+    Character attribute requirements to use the weapon without penalty
+    */
+    attributeRequirement : Record<Attribute, number>;
+
+    /*
+    Scaling amount for attribute
+    */
+    attributeScaling : Record<Attribute, number>;
+
+    /*
+    indicating the base attack rating for each attack type
+    */
+    baseAttack : Record<AttackType, number>;
+
+    /*
+        indicating the base passive effect of the weapon, ­t­±ª¬ºA 
+    */
+    passiveEffect : Record<PassiveEffect, number>;
+}
+
+export interface weaponInfo {
+    weaponWeight : number;
+    weaponLevel : number;
+}
