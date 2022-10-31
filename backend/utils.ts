eaa2d08ee45e0ecd7ba8d782a4c7c698d777ea55
danchang11 <cssource@livemail.tw>
2022-11-01 00:12:24 +0800

    rename temp -> utils

--- backend/utils.ts
+++ backend/utils.ts
@@ -90,43 +90,4 @@ export type Affinitie = typeof allAffinities[number];
 export type AttackType = typeof allAttackTypes[number];
 export type PassiveEffect = typeof allPassiveEffects[number];
 
-export interface weaponAttackResult {
-    attackRating : number;
-}
 
-export interface Weapon{
-    /*
-    Full name of the weapon
-    */
-    weaponName : string;
-
-    /*
-    Regular/Special weapon? (+10 or +25)
-    */
-    isSpecial : boolean;
-
-    /*
-    Character attribute requirements to use the weapon without penalty
-    */
-    attributeRequirement : Record<Attribute, number>;
-
-    /*
-    Scaling amount for attribute
-    */
-    attributeScaling : Record<Attribute, number>;
-
-    /*
-    indicating the base attack rating for each attack type
-    */
-    baseAttack : Record<AttackType, number>;
-
-    /*
-        indicating the base passive effect of the weapon, ­t­±ª¬ºA 
-    */
-    passiveEffect : Record<PassiveEffect, number>;
-}
-
-export interface weaponInfo {
-    weaponWeight : number;
-    weaponLevel : number;
-}
