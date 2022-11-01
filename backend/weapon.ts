397061c267c5b0b687c5bc06e67e7dd73a16e0c7
danchang11 <cssource@livemail.tw>
2022-11-01 21:35:22 +0800

    add attack-scale-att, att-scale-curve

--- backend/weapon.ts
+++ backend/weapon.ts
@@ -1,10 +1,15 @@
-import { AttackType, Attribute, PassiveEffect } from "./utils";
+import { AttackType, Attribute, PassiveEffectsType, weaponType } from './utils';
 
 export interface Weapon{
     /*
     Full name of the weapon
     */
-    weaponName : string;
+    name : string;
+
+    /*
+    weapone metadata
+    */
+    info : weaponInfo ;
 
     /*
     Regular/Special weapon? (+10 or +25)
@@ -16,23 +21,41 @@ export interface Weapon{
     */
     attributeRequirement : Record<Attribute, number>;
 
-    /*
-    Scaling amount for attribute
+    /** 
+    *Scaling amount for attribute
     */
     attributeScaling : Record<Attribute, number>;
 
     /*
+    Map indicating which damage types scale with which player attributes
+    */
+    attackScalingAttributes : Partial<Record<AttackType| number, Attribute[]>>;
+
+    /*
+    Map indicating which scaling curve is used for each damage type
+    */
+    attackScalingCurves: Partial<Record<AttackType| number, WeaponScalingCurve>>;
+
+    /*
     indicating the base attack rating for each attack type
     */
-    baseAttack : Record<AttackType, number>;
+    baseAttack : Partial<Record<AttackType | number, number>>;
 
     /*
         indicating the base passive effect of the weapon, ­t­±ª¬ºA 
     */
-    passiveEffect : Record<PassiveEffect, number>;
+    passiveEffects : Record<PassiveEffectsType, number>;
+
+    /*
+    
+    */
+    paired : boolean;
 }
+
+export type WeaponScalingCurve = 0 | 1 | 2 | 4 | 7 | 8 | 12 | 14 | 15 | 16;
 
 export interface weaponInfo {
     weaponWeight : number;
     weaponLevel : number;
+    weaponType : weaponType;
 }
